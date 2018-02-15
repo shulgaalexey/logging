b6879f53b001accab49af3829584abab5e7feeb3
shulgaalexey <shulgaalexey@gmail.com>
2018-02-15 23:06:32 +0900

    Add logentries appender
    Signed-off-by: shulgaalexey <shulgaalexey@gmail.com>

--- Program.cs
+++ Program.cs
@@ -15,16 +15,21 @@ namespace HelloLogentries {
 
 		static void demo_log4net() {
 			// http://logging.apache.org/log4net/release/manual/configuration.html
+			// https://docs.logentries.com/docs/log4net
 			var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
 			XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
+			// Demo log record
 			LogRecord lr = new LogRecord {
 				Date = $"{DateTime.Now}",
 						 From = "computer",
 						 Msg = "hello computer"
 			};
 
+			// Marshaling log record to json
 			string json = JsonConvert.SerializeObject(lr, Formatting.Indented);
+
+			// Different types of loggin
 			log.Info($"Log info:\n{json}");
 			log.Debug($"Log Debug:\n{json}");
 			Console.WriteLine($"Console WriteLine:\n{json}");
